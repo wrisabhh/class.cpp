dddb15d8af5fed03fd460c415e352e1e42e8d588
rishub <145670392+wrisabhh@users.noreply.github.com>
2023-10-10 01:07:13 +0530

    Add files via upload

---
+++ class1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class readme
+{
+    //properties
+};
+int main()
+{
+
+    readme r1;
+    cout<<"the size of r1 is : " << sizeof(r1)<<endl;
+    return 0;
+}
